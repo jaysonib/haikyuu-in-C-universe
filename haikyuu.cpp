23a9b09ad90a1e5ba639be36043c9627facb0e84
jaysonib <41825536+jaysonib@users.noreply.github.com>
2022-09-30 01:29:11 -0400

    Add files via upload

---
+++ haikyuu.cpp
@@ -0,0 +1,119 @@
+#include<stdio.h>
+#define hinata int
+
+                        /*                                        
+                     @JAYBSONI                                    
+                    @PROGRAMMING                                  
+                   @HKY*/hinata s                                 
+                  ;  hinata main()                                
+                 {hinata key=1<<5-2                               
+                 +1<<5 -2+5*3+5<<1-                               
+                 2<<1-5;key+=(key-1                               
+                );if(key & 1){key-=                               
+                (key%(5*20));;key>>=                              
+                2;;key*=1<<2;}else{}                              
+                hinata c,m,i,j;for(i                              
+                =key+key+key+key+key                              
+                +key-key*6,m=key+i+                               
+                 1<<4+key*(1<<4)+32                               
+                 -1<<5+32-(1<< 6)-                                
+                  16-key-i; c="n"                                 
+                   "-%+=   O0>KB"                                 
+                     "()TANAKA"                "="                
+                       "NOYA "                "="                 
+                                              "=;"                
+                                              " ;"                
+                                          ""  "MB"                
+                                           "  BOK"                
+                                           "YACHIC"               
+                                            "KITAA"               
+                                      "X"   "FAIL"                
+                                 " W! A="    "FAIL"               
+                                 "()TANAKA K ENMAA"               
+                                "-%+B0>KV)<_=  aLM"               
+                               "faF37JAncCDH  UKAI"               
+                               "=fa3L7JnEcCHAr  Ay"               
+                              "faA37JBncCVHr"   "A="              
+                              "KIYKOASAHI(Aa)"  "=A"              
+                               "fa3B7JnDcCHAry" "1C"              
+                               "fa3T7Jn=cCHKr"   "C1"             
+                    "A"        "!ALBAJOES()A"    "1C"             
+                    ""         "!DateTech=()"    "CK"             
+                    ""         "DAICHI=ASAH"     "DOG"            
+                   "A"         "TORU=TSUKII"     "1D1"            
+                    "A"        "fBa37DJncA"   " YxBji"            
+                    "A"         "Tfa3=7JnK  cDCHrAyDs"            
+   ""               "AA"         "ASAHI=DDDEVIL#=JAKLS"           
+   "B"               "A"        "B111A111C111A111C111V"           
+    "B"              "AA"    "=-%+L0>KA)<_AaM<EKJ@Auj"            
+    "BB"              "A"     "LEVHAI=BA;NEKKENMALL"              
+     "K"               "A"   "BOOKKUTO=fa3e7JnBcCH"               
+      "K"              "A" "()=83SeBLACK JJACK=LS"                
+      "cc"              "fa3A7JnAcCHCryDJsbz=a"                   
+       "c"              "fca 3E7JnJcCH=ryDZs"                     
+        ""              "faA 37CJncDCHrJyD"                       
+                       "f= a3V7JnAcCHAryD"                        
+                      "Df  Ba37DJncJCHr="                         
+                      "fa3T7JnAcCHAryDG"                          
+                     " f B a3C7JnJcCH=r"                          
+                    "faU   I37JBncCBHr"                           
+                    " Jf a=37JTncCJay"                            
+                   " Bfa3B7JnJcCH=ry"                             
+                   " TANAKA<>ALBAC;"                              
+                  "faI37J=ncCUHryJD"                              
+                  "jJAYy CjYNIoNc="                               
+                  "fa3N7JnAcCHEryD"                               
+                  "Jfa3B7JnCcCHIry"                               
+                  "f=a37NJncACHrFy"                               
+                 "TtI3t7BomyDSN6H"                                
+                 "JAY=Tt3Mt7oBmySF"                               
+                 "Tt3Ht7oCmySDN6mH"                               
+                 "gK9=Tt3Mt7oBmySF"                               
+                "erJGOpvBkc8FU3FHn"                               
+                "zx=OUrBTP0BHC0Ikb6C"                             
+                "XHSFkzJN5NBH0Ao=9etB"                            
+                "QRuBNqPJEMrBGd5DGQkPO"                           
+                "y1Hsk2=DqSCQqcBE1JJ8Rl"                          
+                "BTt3Ct7oOmySIN6m=gK9De"                   "rJBOp"
+                "JJAYJAYJAAYBSONI(K++)=X"                  " CD0k"
+                "bB6XHJSkzOJ5NHB0A=o9eEtQ"               " BRuNIq"
+               "PEBMrGAd5GJQkOOy1s=k2DOqSQ"            "AqcEB1J8A"
+               "Rl2AqcTH6zNPNgS=g8CP58GAztK"           "AEjpAgaTA"
+               "wcnG64ZQBwD=e3EN4SwAZA4AoHD"           "A6G8ARXr" 
+               "AIDTAn96FZGDRATB=PAGMgFjAcpw"            "C6g9A"  
+              "wmUA6D4FzVHSTKS=ZvlMp7EL1YPSHE"            "A=A"   
+              "rVMzcAKCYwTjgB=nekL6A4KZn1UMbA="                   
+             "bejL3zyKk2rUF3T=RH5LQ1nKdqTUPze=U"                  
+             "aRLXx5KjalUTdk=RNoKLerMp98TwZB=jF"                  
+            "4KFg3LuCgUNkc=SUEKFzuNzSISpoz=gd8Kr"                 
+            "bkO4L0R8ai=DWuKtudPYOBQpeA=jgcK3XRP"                 
+            "3F7ND6jC0D6=2NjJkbFRdWBIJ7cGUwJ=hi1JK  "             
+    "SlYoH  qGpv4=xg4JAp6TYX"   "sHCqVEl3rAtgM=rz5JY3P"           
+   "UKbFMAQC=GOtIMaiVUBHMMN7"     "=  uHBDmX4tALxvF=JAY"          
+   "HAIKYUU-KURO=()-HINAYAM"             "GKXrT=JxODVvJB"         
+   "lTJBUMwKSRzCeqUOkbaGYs"               "t=hUbBx2UBQNSAqt"      
+    "PMBhvIVBHKRglFSKb=x6"                  "FBZe7CTMbA7LhLb"     
+    "n4J6e0LVqjDQNw=kpcCV"                   "JzC9G6AOrnKVM9L"    
+     "9TVKq5K=xCzCzkrDiDF"                   "Adz6JIWQKIgjLQ6"    
+     "TCt3t=7omDySND6mgAK"                    "8FHH3XN   EJAY"    
+      "B9erDJOp=vkcD8U3FF"                     "kuAvx"   "fFff"   
+      "nOzxOCUrTDP0HCC0kB"                      "jay"     "=xx"   
+      "bF6XHES kAzJ5GNB0TAo"                               "dDd"  
+         "so=ni" "RGuNqEPEMA"                              "jay"  
+          "HrGdS5 GCQkO=y1sGk2"                            "fFf"  
+          "DqASQqHc ER1J8CRl2=qc"                           "gG"  
+          "T6zHNNgAS gH8C5P8GzBtK ="                        "eE"  
+          "EGjpgIaTwBc nG64Z=BwDGe "                        "eE"  
+          "3E4ASwZEA4oA  HFD6G=8RXGr"                        ""   
+          "IDETn9C6ZGDDA"  "TABPAGGg"                             
+           "F=jcpHwjcBDwmGU 6CD4zAVHT"                            
+           "EKSZ="   "vlpS7E1BYPHAEe3D  "                         
+           "J=AY"      "cWACYDw j=gBnUek"                         
+            "1B"         "A4ZAn1MC bAQb"                          
+                           "JJ=JAY=Y" ""                          
+                             "JJ=JAY=YY"                          
+                                  "J=JAY"                         
+[(!(c-61)?(m=1<<5,printf("\n")):(m^=1)),i+=(4<<0)];){for(j=0;(c-61)
+&&(j++-c-(c<64?5:(c>90?-70:-64)));printf("%c",m));}
+return 0;
+}   
